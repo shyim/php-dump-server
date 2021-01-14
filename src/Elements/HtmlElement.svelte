7edbd2e7dc7f1ab657ae066c6fa7fb7a3defce03
Soner Sayakci <s.sayakci@shopware.com>
2021-01-14 10:35:52 +0100

    Allow usage of JS in HtmlElement

--- src/Elements/HtmlElement.svelte
+++ src/Elements/HtmlElement.svelte
@@ -1,5 +1,24 @@
 <script>
+    import { onMount } from 'svelte';
     export let content;
+    export const id = '_' + Math.random().toString(36).substr(2, 9);
+
+    const setInnerHTML = function(elm, html) {
+        elm.innerHTML = html;
+
+        Array.from(elm.querySelectorAll('script')).forEach(oldScript => {
+            const newScript = document.createElement('script');
+
+            Array.from(oldScript.attributes)
+                .forEach( attr => newScript.setAttribute(attr.name, attr.value) );
+            newScript.appendChild(document.createTextNode(oldScript.innerHTML));
+            oldScript.parentNode.replaceChild(newScript, oldScript);
+        });
+    }
+
+    onMount(() => {
+        setInnerHTML(document.getElementById(id), content)
+    });
 </script>
 
-{@html content}
+<div id={id}></div>
